040e81fa4dfd9af8db3e26b0118d5fcb17d43224
Aksel Kvitberg <aksel.kvitberg@outlook.com>
2021-02-23 23:39:22 +0100

    Init

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PhysicsObject : MonoBehaviour
+{
+    public float minGroundNormalY = .65f;
+    public float gravityModifier = 1f;
+    public float maxFallSpeed = 10;
+
+    protected float horizontalVelocity;
+    protected bool grounded;
+    protected WallHit wallHit;
+    protected Vector2 groundNormal;
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected readonly RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.02f;
+
+    protected float currentGravityModifier = 0;
+
+    protected bool customGravity = false;
+
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask (Physics2D.GetLayerCollisionMask (gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+    private void Update()
+    {
+        horizontalVelocity = 0;
+        currentGravityModifier = gravityModifier;
+        ComputeVelocity ();
+    }
+
+    protected virtual void ComputeVelocity()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        velocity += Physics2D.gravity * (currentGravityModifier * Time.deltaTime);
+
+        velocity.y = Mathf.Max(-maxFallSpeed, velocity.y);
+        velocity.x = horizontalVelocity;
+
+        grounded = false;
+        wallHit = WallHit.None;
+
+        var deltaPosition = velocity * Time.deltaTime;
+
+        var moveAlongGround = new Vector2 (groundNormal.y, -groundNormal.x);
+
+        var move = moveAlongGround * deltaPosition.x;
+
+        Movement (move, false);
+
+        move = Vector2.up * deltaPosition.y;
+
+        Movement (move, true);
+
+        PostFixedUpdate();
+
+    }
+
+    protected virtual void PostFixedUpdate()
+    {
+        
+    }
+
+    void Movement(Vector2 move, bool yMovement)
+    {
+        var distance = move.magnitude;
+
+        if (distance > minMoveDistance) 
+        {
+            var count = rb2d.Cast (move, contactFilter, hitBuffer, distance + shellRadius);
+
+            for (var i = 0; i < count; i++) 
+            {
+                var currentNormal = hitBuffer [i].normal;
+                if (currentNormal.y > minGroundNormalY) 
+                {
+                    grounded = true;
+                    if (yMovement) 
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                if (currentNormal.y == 0)
+                {
+                    wallHit = currentNormal.x > 0 ? WallHit.Left : WallHit.Right;
+                }
+
+                var projection = Vector2.Dot (velocity, currentNormal);
+                if (projection < 0) 
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                var modifiedDistance = hitBuffer [i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+            
+        }
+
+        rb2d.position = rb2d.position + move.normalized * distance;
+    }
+
+}
+
+public enum WallHit
+{
+    None,
+    Left,
+    Right
+}
---
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class PlayerPlatformerController : PhysicsObject
+{
+    public float moveSpeed = 7;
+    
+    public float jumpTakeOffSpeed = 7;
+    [Tooltip("Gravity applied when space bar is held")]
+    public float highJumpGravityModifier = 1f;
+
+    [Header("Jump forgiveness")]
+    public float coyoteTime = 0.2f;
+    private float coyoteTimeCounter = 0;
+
+    public float jumpBuffer = 0.1f;
+    private float jumpBufferCounter = 0;
+    private float wallJumpBufferCounter = 0;
+
+    [Header("Wall jump")]
+    public Vector2 walljumpSpeed = new Vector2(4,7);
+    public float wallGrabSlideSpeed = -1.5f;
+
+
+    // Freeze direction in case of a slide or wall jump
+    private Vector2 directionFreeze = Vector2.zero;
+    private float directionFreezeTime = 0.15f;
+    private float directionFreezeCounter = 0;
+    
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private static readonly int Grounded = Animator.StringToHash("grounded");
+    private static readonly int Speed = Animator.StringToHash("speed");
+    private static readonly int Verticalspeed = Animator.StringToHash("verticalspeed");
+    private static readonly int Hang = Animator.StringToHash("hang");
+
+    void Awake () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer> ();    
+        animator = GetComponent<Animator> ();
+    }
+    
+    protected override void ComputeVelocity()
+    {
+        BufferJumpInput();
+        
+        if (CanMove())
+        {
+            DoGravity();
+            
+            // Player input movement
+            DoJump();
+            
+            var x = Input.GetAxisRaw("Horizontal");
+            horizontalVelocity = x * moveSpeed;
+
+            DoWallSlide(x);
+        }
+        else
+        {
+            horizontalVelocity = directionFreeze.x;
+            velocity.y = directionFreeze.y;
+        }
+        
+        FlipSprite(horizontalVelocity);
+
+        animator.SetBool (Grounded, grounded);
+        animator.SetFloat (Speed, Mathf.Abs (horizontalVelocity) / moveSpeed);
+        animator.SetFloat (Verticalspeed, velocity.y);
+    }
+
+    private void DoWallSlide(float x)
+    {
+        if (!grounded && velocity.y <= 0 && (wallHit == WallHit.Left && x < 0 || wallHit == WallHit.Right && x > 0))
+        {
+            wallJumpBufferCounter = jumpBuffer;
+            velocity.y = wallGrabSlideSpeed;
+            currentGravityModifier = 0;
+            animator.SetBool(Hang, true);
+        }
+        else
+        {
+            animator.SetBool(Hang, false);
+        }
+
+        if (wallJumpBufferCounter > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                wallJumpBufferCounter = 0;
+                velocity.y = walljumpSpeed.y;
+                directionFreezeCounter = directionFreezeTime;
+                directionFreeze = new Vector2(wallHit == WallHit.Left ? walljumpSpeed.x : -walljumpSpeed.x, walljumpSpeed.y);
+                wallHit = WallHit.None;
+                animator.SetBool(Hang, false);
+            }
+            else
+                wallJumpBufferCounter -= Time.deltaTime;
+        }
+    }
+
+    private void FlipSprite(float x)
+    {
+        if (x != 0)
+        {
+            spriteRenderer.flipX = x < 0f;
+            //Debug.Log(spriteRenderer.flipX);
+        }
+    }
+
+    private bool CanMove()
+    {
+        if (directionFreezeCounter <= 0)
+            return true;
+        directionFreezeCounter -= Time.deltaTime;
+        return false;
+    }
+
+    private void DoJump()
+    {
+        if (jumpBufferCounter > 0 && coyoteTimeCounter > 0) 
+        {
+            velocity.y = jumpTakeOffSpeed;
+            jumpBufferCounter = 0;
+            coyoteTimeCounter = 0;
+        }
+    }
+
+    private void DoGravity()
+    {
+        if (Input.GetKey(KeyCode.Space) && (velocity.y > 0 ))
+        {
+            currentGravityModifier = highJumpGravityModifier;
+        }
+        else
+        {
+            currentGravityModifier = gravityModifier;
+        }
+    }
+    
+    private void BufferJumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferCounter = jumpBuffer;
+        }
+        else if (jumpBufferCounter > 0)
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+    }
+    
+    protected override void PostFixedUpdate()
+    {
+        // Must buffer coyote time after movement has happened in fixed update, not in update.
+        // If not, we would register ground after a jump has occurred, even though we haven't moved yet,
+        // granting the player a double jump
+        BufferCoyoteTime();
+    }
+    
+    private void BufferCoyoteTime()
+    {
+        if (grounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else if (coyoteTimeCounter > 0)
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+    }
+}
